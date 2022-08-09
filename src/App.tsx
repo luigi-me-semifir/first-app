47656af1b72b138f8ec423636229a3823c8efa72
lume-semifir <luigi.menez@semifir.com>
2022-08-09 11:04:10 +0200

    Add components Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header';
-
+import Footer from './components/Footer/Footer';
 import './App.css';
+
 
 function App() {
   return (  
     <>
       <Header />
+      <Footer />
     </>
   )
   }
