bde271391d863dcd9c935b693bfa6b058999bc90
lume-semifir <luigi.menez@semifir.com>
2022-08-09 14:41:00 +0200

    Creation of the detailsUser page with the link that directs to it.

--- src/components/MemberList/MemberList.tsx
+++ src/components/MemberList/MemberList.tsx
@@ -1,32 +1,26 @@
 import { useEffect, useState } from "react";
 import User from "../../models/user";
 import axios from "axios";
+import { Link } from "react-router-dom";
 // import UserAPi from "../../services/userAPI";
 
 function MemberList() {
   const [members, setMembers] = useState<User[]>([]);
 
   useEffect(() => {
-    // allUsers();
-
     axios.get(`http://localhost:8000/members`).then(({ data }) => {
       setMembers(data);
     });
   }, []);
 
-  // const allUsers = async () => {
-  //   const data = await UserAPi.findAll();
-  //   setMembers(data);
-  // };
-
   return (
     <>
       {members.map((member) => (
-        <section className="meberCards" key={member.id}>
+        <Link to={`/${member.id}`} className="meberCards" key={member.id}>
           <p>Nom: {member.lastname}</p>
           <p>Prénom: {member.firstname}</p>
           <p>Adresse: {member.adress}</p>
-        </section>
+        </Link>
       ))}
     </>
   );
---
+++ src/pages/DetailsUser.tsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+
+function DetailsUser() {
+  const { id } = useParams();
+  return <h1>Hello {id}</h1>;
+}
+
+export default DetailsUser;
